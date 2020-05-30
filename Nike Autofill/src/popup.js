7a2b411b4149f3efa2e5943dc4f203d71f8eb77e
RomanDovgii <romandovgiiwork@gmail.com>
2020-05-30 23:56:49 +0300

    fixed tabs switching problem
    You shall not use javaScript inside of your HTML file.

--- Nike Autofill/src/popup.js
+++ Nike Autofill/src/popup.js
@@ -1,66 +1,104 @@
-var currentTab = 0;
-var tabs = document.getElementsByClassName("tab");
-re_passportNumber = '';
-showTab(currentTab); 
+const nextBtn = document.querySelector(`#nextBtn`);
+const prevBtn = document.querySelector(`#prevBtn`);
 
+let tabs = document.querySelectorAll(`.tab`);
+let currentTab = 0; 
 
-function showTab(n) {
+const showTab = (tabNumber) => {
+  this.tabs = tabs;
+  this.tabs[tabNumber].style.display = "grid";
+  
+  tabNumber === 0 ? hide(prevBtn) : show(prevBtn);
+  tabNumber === (this.tabs.length - 1) ? changeText(nextBtn, `Set Profile`) : changeText(nextBtn, `Continue`);
+  console.log("wooden dicks");
+}
 
-  this.tabs = document.getElementsByClassName("tab");
-  this.tabs[n].style.display = "grid";
+const hide = (element) => {
+  element.style.display = `none`;
+}
 
-  if (n == 0) {
-    document.getElementById("prevBtn").style.display = "none";
-  } else {
-    document.getElementById("prevBtn").style.display = "inline";
+const show = (element) => {
+  element.style.display = `inline`;
+}
+
+const changeText = (element, text) => {
+  element.innerHTML = text;
+}
+
+const nextTab = () => {
+  hide(tabs[currentTab]);
+  currentTab++;
+  console.log(currentTab);
+
+  if (currentTab > (tabs.length - 1)) {
+    currentTab = tabs.length - 1;
   }
 
-  if (n == (this.tabs.length - 1)) {
-    document.getElementById("nextBtn").innerHTML = "Set profile";
-  } else {
-    document.getElementById("nextBtn").innerHTML = "Continue";
+  showTab(currentTab);
+}
+
+prevTab = () => {
+  hide(tabs[currentTab]);
+  currentTab--;
+  console.log(currentTab);
+
+  if (currentTab < 0) {
+    currentTab = 0;
   }
 
+  showTab(currentTab);
 }
 
-function nextPrev(i) {
-    console.log('okko')
-  this.tabs = document.getElementsByClassName("tab");
-  this.tabs[this.currentTab].style.display = "none";
-  if (validateTab(this.tabs[this.currentTab])){
+//testing range
 
-    this.currentTab = this.currentTab + i;
-    if (this.currentTab >= this.tabs.length) {
-        document.getElementById("regForm").submit();
-        return false;
-    }
-  }
-  showTab(this.currentTab);
-}
+showTab(currentTab);
+nextBtn.addEventListener('click', () => {
+  nextTab(1);
+});
+prevBtn.addEventListener('click', () => {
+  prevTab(1);
+});
 
-function validateTab(tab){
-    let inputs = tab.getElementsByTagName("input")
-    let valid = true;
+//Эту хуйню доделаешь сам - мне за это не платили. 
 
-    for (let i = 0; i < inputs.length; i++){
+// function nextPrev(i) {
+//     console.log('okko')
+//   this.tabs = document.getElementsByClassName("tab");
+//   this.tabs[this.currentTab].style.display = "none";
+//   if (validateTab(this.tabs[this.currentTab])){
 
-        // if (inputs[i].value == "" && inputs[i].id !== 'address2' ) return false;
-        // else if ( inputs[i].id == 'passportNumber'  ){}
-        if (!inputs[i].checkValidity()){
-            inputs[i].className += " invalid";
-            valid = false;
-        }else {
-            inputs[i].className -= " invalid";
+//     this.currentTab = this.currentTab + i;
+//     if (this.currentTab >= this.tabs.length) {
+//         document.getElementById("regForm").submit();
+//         return false;
+//     }
+//   }
+//   showTab(this.currentTab);
+// }
 
-            continue;
-        }
-    }
-    return valid;
-}
+// function validateTab(tab){
+//     let inputs = tab.getElementsByTagName("input")
+//     let valid = true;
 
-function out(){
-    console.log('okokok');
-}
+//     for (let i = 0; i < inputs.length; i++){
+
+//         // if (inputs[i].value == "" && inputs[i].id !== 'address2' ) return false;
+//         // else if ( inputs[i].id == 'passportNumber'  ){}
+//         if (!inputs[i].checkValidity()){
+//             inputs[i].className += " invalid";
+//             valid = false;
+//         } else {
+//             inputs[i].className -= " invalid";
+
+//             continue;
+//         }
+//     }
+//     return valid;
+// }
+
+// function out(){
+//     console.log('okokok');
+// }
 
 
 
